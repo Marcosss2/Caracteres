4b2ec5d5c4d03cbd434595f47f16fc4bfde34013
Marcosss2 <marcosparra20@hotmail.com>
2021-09-23 10:13:19 +0200

    nuevo

---
+++ caracteres/src/caracteres/Caracteres.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caracteres;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Caracteres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         var alumno = "Marcos";
+         var titulo = "informatico";
+
+         var concat = titulo + " " + alumno;
+
+        System.out.println("concat = " + concat);
+
+        System.out.println("Tabulador \t" + alumno);
+        System.out.println("Retroceso \b" + alumno);
+        System.out.printf("Salto de línea \n" + alumno);
+    
+    
+    
+    }
+    
+}
